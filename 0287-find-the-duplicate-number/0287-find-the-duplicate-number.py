55e167716ccb33722d0cd0f816ecf489c00c1089
Umesh kumar  Sutihar <umeshsutihar000@gmail.com>
2026-09-12 21:05:30 +0530

    Time: 25 ms (62.24%), Space: 33.6 MB (50.35%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -0,0 +1,22 @@
+class Solution:
+    def findDuplicate(self, nums):
+        slow = nums[0]
+        fast = nums[0]
+
+        # Find intersection point
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+
+            if slow == fast:
+                break
+
+        # Find duplicate number
+        slow = nums[0]
+
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[fast]
+
+        return slow
+        
